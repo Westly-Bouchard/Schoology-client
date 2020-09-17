8a9ec9505c489d9b69de3b70b4bbbbfabaf9b0fe
West <weslty.bouchard@gmail.com>
2020-09-16 23:03:54 -0600

    Wrote functions for generating the oauth header.

--- src/main/java/Oauth.java
+++ src/main/java/Oauth.java
@@ -1,45 +1,93 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.awt.Desktop;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.*;
 
 public class Oauth {
     private final String consumerKey = ClientCred.CONSUMER_KEY;
     private final String secretKey = ClientCred.SECRET_KEY;
 
-    //callback should always be oob
-    private static String makeBaseString(String url, String method, HashMap<String, String> oauthHeader) {
-        return null;
+    private static String tokenKey = "";
+    private static String tokenSecret = "";
+
+
+
+    private static String makeBaseString(String uri, String method, TreeMap<String, String> oauthHeader) throws UnsupportedEncodingException {
+        String returnString = method.toUpperCase();
+        returnString += "&";
+        returnString += URLEncoder.encode("https://api.schoology.com/v1/" + uri, StandardCharsets.UTF_8.toString());
+        returnString += "&";
+
+        for(Map.Entry<String, String> entry : oauthHeader.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            returnString = returnString.concat(URLEncoder.encode(entry.getKey() + "=" + entry.getValue(), StandardCharsets.UTF_8.toString()));
+        }
+
+
+        return returnString;
     }
 
-    private static String makeOauthSig(String url, String method, HashMap<String, String> oauthHeader) {
-        //Will call makeBaseString()
-        return null;
+    //For the makeOauthSig method
+    private static String toHexString(byte[] bytes) {
+        Formatter formatter = new Formatter();
+
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+
+        return formatter.toString();
     }
 
-    public static String makeOauthHeader(String url, String method, String body) {
-        HashMap<String, String> oauthHeader;
-        //Initialize the hashmap with all necessary values in the Authorization header
+    //For the makeOauthSig method: Generates the HMAC-SHA1 hash
+    private static String calculateRFC2104HMAC(String data, String key)
+            throws NoSuchAlgorithmException, InvalidKeyException
+    {
+        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(signingKey);
+        return toHexString(mac.doFinal(data.getBytes()));
+    }
 
-        /*
-        Call makeOauthSig passing the HashMap, url, and method (is a string)
-        Iterate through the HashMap and assemble the oauth header
-        return the header in the form of a string
-         */
+    private static String makeOauthSig(String url, String method, TreeMap<String, String> oauthHeader)
+            throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, UnsupportedEncodingException {
+        String baseString = makeBaseString(url, method, oauthHeader);
+        String key = URLEncoder.encode((ClientCred.SECRET_KEY + "&" + tokenSecret), StandardCharsets.UTF_8.toString());
+        return calculateRFC2104HMAC(baseString, key);
+    }
 
-        /* Just some testing code that will be useful when writing the actual method
-        long timestamp = System.currentTimeMillis() / 1000;
+    public static String makeOauthHeader(String uri, String method) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        //Create the timestamp and nonce
+        long timestamp = System.currentTimeMillis()/1000;
         UUID nonce = UUID.randomUUID();
 
-        String oauthHeader = "Oauth realm=\"Schoology API\"," +
-                "oauth_consumer_key=" + ClientCred.CONSUMER_KEY + "," +
-                "oauth_nonce=\"" + nonce + "\"," +
-                "oauth_signature_method=\"HMAC-SHA1\"," +
-                "oauth_timestamp=\"" + timestamp + "\"," +
-                "oauth_token=\"\"," +
-                "oauth_version=\"1.0\"," +
-                "oauth_signature=\"" + ClientCred.SECRET_KEY + "\"";
-         */
-        return null;
+        //Initialize the hashmap with all necessary values in the Authorization header
+        TreeMap<String, String> oauthHeader = new TreeMap<String, String>() {{
+           put("oauth_consumer_key", ClientCred.CONSUMER_KEY);
+           put("oauth_nonce", nonce.toString());
+           put("oauth_signature_method", "HMAC-SHA1");
+           put("oauth_timestamp", String.valueOf(timestamp));
+           put("oauth_token", tokenKey);
+           put("oauth_callback", "oob");
+           put("oauth_version", "1.0");
+        }};
+
+
+        //Call makeOauthSig passing the HashMap, url, and method (is a string)
+        oauthHeader.put("oauth_signature", makeOauthSig(uri, method, oauthHeader));
+
+        //Iterate through the HashMap and assemble the oauth header
+        String returnHeader = "Oauth realm=\"Schoology API\",";
+        for(Map.Entry<String, String> entry : oauthHeader.entrySet()) {
+            returnHeader = returnHeader.concat(entry.getKey() + "=\"" + entry.getValue() + "\",");
+        }
+        return returnHeader;
     }
 
     // /oauth/request_token
